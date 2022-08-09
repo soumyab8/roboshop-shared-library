16dd63a45541178ec89484d2fec6e354f25a613a
User <soumya.bhangi@gmail.com>
2022-08-09 23:27:32 +0530

    new file

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -9,9 +9,9 @@ def lintChecks() {
 def call() {     // call is the default which will be called
 pipeline {
     agent any 
-    environment { 
-        SONAR = credentials('sonar')
-    }
+    // environment { 
+    //     SONAR = credentials('sonar')
+    // }
     stages {
         // This should run for every commit on feature branch
         stage('Lint checks') {
@@ -22,18 +22,18 @@ pipeline {
                 }
             }
 
-        stage('Sonar Code Quality Check') {
-            steps {
-                script {
-                     common.javaSonarCheck()
-                    }
-                }
-            }
-        stage('Build') {
-            steps {
-                sh "echo Doing build"
-               }
-            }
+        // stage('Sonar Code Quality Check') {
+        //     steps {
+        //         script {
+        //              common.javaSonarCheck()
+        //             }
+        //         }
+        //     }
+        // stage('Build') {
+        //     steps {
+        //         sh "echo Doing build"
+        //        }
+        //     }
         } // end of the stages
     }  // end of the pipeline
 }  // end of function call 
