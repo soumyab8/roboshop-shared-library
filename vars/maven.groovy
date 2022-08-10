14af5a59d7c3945ca70754492e2be3ef95a3f557
User <soumya.bhangi@gmail.com>
2022-08-10 21:36:09 +0530

    new file

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -31,6 +31,7 @@ pipeline {
         stage('SONAR checks') {
             steps {
                 script {
+                    sh mvn clean compile
                     sonarCheck()
                     }
                 }
