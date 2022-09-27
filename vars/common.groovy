1fb2aea0aa9db84d6cf2cc4813f8e0b18e2dd1a8
User <soumya.bhangi@gmail.com>
2022-09-27 10:33:18 +0530

    new file

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,16 +1,16 @@
 def javaSonarCheck() {
   sh '''
-    mvn clean compile
-    sonar-scanner -Dsonar.host.url=http://172.31.12.77:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar.java.binaries=target/classes/
-    curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
-    chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.12.77 ${COMPONENT}
+    # mvn clean compile
+    # sonar-scanner -Dsonar.host.url=http://172.31.12.77:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar.java.binaries=target/classes/
+    # curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
+    # chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.12.77 ${COMPONENT}
    '''
 }
 
 def sonarCheck() {
   sh '''
-    sonar-scanner -Dsonar.host.url=http://172.31.12.77:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT}
-    curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
-    chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.12.77 ${COMPONENT}
+    # sonar-scanner -Dsonar.host.url=http://172.31.12.77:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT}
+    # curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
+    # chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.12.77 ${COMPONENT}
    '''
 }
