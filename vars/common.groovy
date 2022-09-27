1b620baa723ed71aec627c496e3ff38bb708be54
User <soumya.bhangi@gmail.com>
2022-09-27 13:20:12 +0530

    new file

--- vars/common.groovy
+++ vars/common.groovy
@@ -4,6 +4,7 @@ def javaSonarCheck() {
     # sonar-scanner -Dsonar.host.url=http://172.31.12.77:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar.java.binaries=target/classes/
     # curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
     # chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.12.77 ${COMPONENT}
+    echo sonar checks completed
    '''
 }
 
@@ -12,5 +13,6 @@ def sonarCheck() {
     # sonar-scanner -Dsonar.host.url=http://172.31.12.77:9000 -Dsonar.sources=. -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT}
     # curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > /tmp/quality-gate.sh 
     # chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.12.77 ${COMPONENT}
+     echo sonar checks completed
    '''
 }
